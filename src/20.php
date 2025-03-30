f380a0726d9bc6e6b7d4e520344f39529f02c260
burrussvukcevic958 <burrussvukcevic958@gmail.com>
2025-03-30 19:56:14 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,10 @@
+<?php
+// Example code to demonstrate basic PHP functionality
+
+// Define an array of some values
+$array = ['value1', 'value2', 'value3'];
+
+// Loop through the array and print each value
+foreach ($array as $value) {
+    echo $value . "\n";
+}
